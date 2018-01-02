5e3414e1032f35c8ef55a81130507d4dcf1e495b
Cem Galioglu <cgalioglu13@ku.edu.tr>
2018-01-02 21:28:14 +0300

    Deleting unnecessary file

--- cutWhiteSide.m
+++ cutWhiteSide.m
@@ -1,11 +1,11 @@
 function [trimmedImage] = cutWhiteSide(image)
 %CUTWH�TEPARTS Summary of this function goes here
 %   Detailed explanation goes here
-sums = sum (not(image));
+sums = sum(not(image));
 elementx = 0;
 counter = 0;
 while elementx == 0
     counter = counter + 1;
     elementx =  sums(counter);
 end
-trimmedImage =  image(:,counter : size(image,2));
+trimmedImage =  image(:,countoer : size(image,2));
